3d6d228e4355c30c87d9cb6b0f4a0908c3c45de7
Alex <7up-des@mail.ru>
2015-03-06 12:59:10 +0200

    third commit (added junit)

---
+++ src/main/java/school/App.java
@@ -0,0 +1,26 @@
+package school;
+
+/**
+ * Created by Alex on 05.03.2015.
+ */
+public class App {
+    public static void main(String[] args) {
+        Student sasha = new Student("Denysov", "Sasha");
+        Student stepa = new Student("Denysov", "Stepan");
+        Student igor = new Student("Denysov", "igor");
+        SchoolClass first = new SchoolClass();
+        Organizer organizer = new Organizer();
+        organizer.addNewStudent(sasha, first);
+        organizer.addNewStudent(stepa, first);
+        organizer.addNewStudent(igor, first);
+        System.out.println(first.toString());
+
+        SchoolClass second = new SchoolClass();
+        Student yura = new Student("Denysov", "yura");
+        Student yula = new Student("Denysov", "yula");
+        organizer.addNewStudent(yura, second);
+        organizer.addNewStudent(yula, second);
+        System.out.println(second.toString());
+
+    }
+}
---
+++ src/main/java/school/Organizer.java
@@ -0,0 +1,40 @@
+package school;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Alex on 05.03.2015.
+ */
+public class Organizer implements MusicSchoolOrganizer{
+
+
+
+
+    @Override
+    public void addNewStudent(Student student, SchoolClass schoolClass) {
+        schoolClass.classes.add(student);
+    }
+
+    @Override
+    public void deleteStudentFromClass(Student student, SchoolClass schoolClass) {
+
+    }
+
+    @Override
+    public List<Student> getAllStudents() {
+        return null;
+    }
+
+    @Override
+    public List<Student> getAllStudentsFromClass(SchoolClass schoolClass) {
+        return null;
+    }
+
+    @Override
+    public void schedulePerformance(Performance performance) {
+
+    }
+
+
+}
